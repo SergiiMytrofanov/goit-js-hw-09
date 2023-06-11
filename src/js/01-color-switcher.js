1493981e8ce8707e812215785db07ef0b7cf2e77
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-06-11 15:35:45 +0300

    first task commit
    background colour changing script added on button click

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+startBtn.addEventListener('click', () => {
+
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+   intervalId = setInterval(() =>{
+    document.body.style.backgroundColor = getRandomHexColor();
+}, 1000); 
+});
+
+stopBtn.addEventListener('click', () => {
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    clearInterval(intervalId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  };
