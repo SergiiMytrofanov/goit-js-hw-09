f339ab95f312c078599eb50b3086953d01ae3a5f
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-06-12 22:41:10 +0300

    task 3 commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delayInput = document.querySelector('.js-delay');
+const stepInput = document.querySelector('.js-step');
+const amountInput = document.querySelector('.js-amount');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        Notiflix.Notify.success(`✅ Виконана обіцянка ${position} з затримкою ${delay}ms`);
+        resolve({ position, delay });
+      } else {
+        Notiflix.Notify.failure(`❌ Невиконана обіцянка ${position} з затримкою ${delay}ms`);
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const delay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  let currentDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, currentDelay);
+    currentDelay += step;
+  }
+});
+
